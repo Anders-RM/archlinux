ac7abf2350b11e1f10620c0d887370432afcfb2d
Anders-RM <usual.dusk6145@fastmail.com>
2024-10-23 13:15:15 +0200

    test new launch Filen

--- appimage.sh
+++ appimage.sh
@@ -56,7 +56,7 @@ run_command "sudo cp /usr/share/applications/filen-desktop.desktop $HOME/.config
 run_command "mkdir -p $HOME/filen" "Creating filen directory"
 run_command "ln -s $HOME/filen $HOME/Desktop/Filen" "Creating desktop shortcut"
 log "Filen AppImage installed successfully"
-konsole --noclose -e bash -c "gio launch /usr/share/applications/filen-desktop.desktop; exec bash" &
+run_command "gio launch /usr/share/applications/filen-desktop.desktop; exec bash &" "Launching Filen"
 wait $!
 
 log "Filen launched successfully"
