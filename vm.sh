3ba13b893803013c6c209e4f9772bf5d2530a070
Anders-RM <usual.dusk6145@fastmail.com>
2024-10-24 07:09:19 +0200

    Refactor vm.sh script to handle disabled virtualization and prompt user for installation continuation

--- vm.sh
+++ vm.sh
@@ -24,6 +24,7 @@ run_command() {
 }
 
 # Check if the CPU supports virtualization
+log "Checking if virtualization is enabled"
 if [ "$(grep -Ec '(vmx|svm)' /proc/cpuinfo)" -gt 0 ]; then
     log "Virtualization Enabled"
     run_command "sudo pacman -Syyu --noconfirm" "Updating system"
@@ -36,7 +37,29 @@ if [ "$(grep -Ec '(vmx|svm)' /proc/cpuinfo)" -gt 0 ]; then
     run_command "sudo systemctl restart libvirtd.service" "Restarting libvirtd service"
 else
     log "Virtualization Disabled"
-    exit 1
+    read -p "Virtualization is disabled. Do you want to continue with the installation? (y/n): " choice
+    case "$choice" in 
+        y|Y ) 
+            log "Continuing with installation"
+            run_command "sudo pacman -Syyu --noconfirm" "Updating system"
+            run_command "sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq bridge-utils libguestfs ebtables vde2 openbsd-netcat" "Installing VM packages"
+            run_command "sudo systemctl enable libvirtd.service" "Enabling libvirtd service"
+            run_command "sudo systemctl start libvirtd.service" "Starting libvirtd service"
+            run_command "sudo sed -i '/^#.*unix_sock_group = \"libvirt\"/s/^#//' /etc/libvirt/libvirtd.conf" "Setting libvirt group"
+            run_command "sudo sed -i '/^#.*unix_sock_rw_perms = \"0770\"/s/^#//' /etc/libvirt/libvirtd.conf" "Setting libvirt permissions"
+            run_command "sudo usermod -aG libvirt $USER" "Adding user to libvirt group"
+            run_command "sudo systemctl restart libvirtd.service" "Restarting libvirtd service"
+            ;;
+        n|N ) 
+            log "Installation aborted by user"
+            exit 1
+            ;;
+        * ) 
+            log "Invalid choice. Installation aborted."
+            exit 1
+            ;;
+    esac
 fi
+
 
 exit 0
