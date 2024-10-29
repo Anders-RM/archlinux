dbcb12ba6511a740e54bd119acdc899e397fbebb
Anders-RM <usual.dusk6145@fastmail.com>
2024-10-29 10:52:55 +0100

    RM Brave

--- app_install.sh
+++ app_install.sh
@@ -27,7 +27,7 @@ run_command() {
 pacmanId=("vlc" "flatpak" "python-beautifulsoup4" "python-lxml" "fuse2" "axel" "aria2" "kio-admin")
 cloneRepos=("https://aur.archlinux.org/yay.git")
 repoDirs=("yay")
-yayId=("snapd" "bauh" "1password" "brave-bin")
+yayId=("snapd" "bauh" "1password")
 flatpakId=("")
 snapId=()
 
