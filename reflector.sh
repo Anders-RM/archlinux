c046ddd740191c8d3e47f3ab2fedbe1b7d925436
Anders-RM <usual.dusk6145@fastmail.com>
2024-11-01 10:14:40 +0100

    Refactor reflector.timer configuration modification to use run_command and reload systemd daemon

--- reflector.sh
+++ reflector.sh
@@ -62,7 +62,6 @@ TIMER_CONF=/etc/systemd/system/timers.target.wants/reflector.timer
 run_command "sudo systemctl enable --now reflector.timer" "Enabling and starting reflector timer"
 run_command "sudo systemctl enable --now reflector.service" "Enabling and starting reflector service"
 
-sudo sed -i 's/^OnCalendar=weekly/OnCalendar=\*-*-* 18:00:00/' "$TIMER_CONF"
-log "Modified reflector.timer configuration"
-
+run_command "sudo sed -i 's/^OnCalendar=weekly/OnCalendar=\*-*-* 18:00:00/' \"$TIMER_CONF\"" "Modified reflector.timer configuration"
+run_command "sudo systemctl daemon-reload" "Reloading systemd daemon"
 exit 0
