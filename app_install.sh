343f0059dd75b6874ad9f411f8e3fca663e9ffbe
Anders-RM <usual.dusk6145@fastmail.com>
2024-11-01 07:44:56 +0100

    Update 1Password launch command to use konsole and prevent terminal closure

--- app_install.sh
+++ app_install.sh
@@ -88,7 +88,7 @@ done
 
 # Final setup for 1Password
 echo "Setup 1Password: Enable SSH agent under the developer settings."
-run_command "gio launch /usr/share/applications/1password.desktop; exec bash &" "Launching 1password"
+run_command "konsole --noclose -e bash -c 'gio launch /usr/share/applications/1password.desktop; exec bash'" "Launching 1password"
 wait $!
 read -p "Press enter to continue"
 log "Killing 1Password"
